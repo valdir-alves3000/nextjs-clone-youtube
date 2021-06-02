7669182bb1a6e5bdcf9238ab1002febe281aa9b4
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2021-06-02 10:46:46 -0300

    Config MulterS3

---
+++ src/pages/api/videos.js
@@ -0,0 +1,17 @@
+import nc from 'next-connect';
+import upload from '../../utils/upload';
+
+
+const handler = nc()
+  .use(upload.single('file'))  
+  .post((req, res) => {
+    const { title, authorName, authorAvatar, videoUrl } = req.body;
+
+    res.json({ hello: 'World' });
+  })
+ 
+  .patch(async (req, res) => {
+    throw new Error("Throws me around! Error can be caught and handled.");
+  });
+
+export default handler;
